9604c3738cb513182dee1d410d155e9419ca6ea0
Stavros Vologiannidis <svologia@gmail.com>
2025-05-13 20:14:59 +0300

    Initial commit

--- snippets/permissions.py
+++ snippets/permissions.py
@@ -8,3 +8,5 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
             return True
         # Write permissions are only allowed to the owner of the snippet.
         return obj.owner == request.user
+
+
